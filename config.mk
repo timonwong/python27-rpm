3b4a8a222fb658202beb31c2a4550e3d5b2c89e5
Timon Wong <timon86.wang@gmail.com>
2014-09-17 23:39:37 +0800

    Fix line endings and misc fix

--- config.mk
+++ config.mk
@@ -5,7 +5,7 @@
 ### Package name for python
 PYTHON_NAME=python27
 PYTHON_MAJOR_VERSION=2.7
-PYTHON_FULL_VERSION=2.7.6
+PYTHON_FULL_VERSION=2.7.8
 
 ### Package rpm file name
 PYTHON_RPM=$(PYTHON_NAME)-$(PYTHON_FULL_VERSION)-1.$(shell uname -m).rpm
@@ -20,12 +20,12 @@ PYTHON_SOURCE_TARBALL_DOWNLOAD_MIRROR=http://mirrors.sohu.com/python
 
 ## Python libraries & tools configurations
 
-SETUPTOOLS_VER=2.2
-VIRTUALENV_VER=1.11.4
-PIP_VER=1.5.4
+SETUPTOOLS_VER=5.7
+VIRTUALENV_VER=1.11.6
+PIP_VER=1.5.6
 MELD3_VER=1.0.0
-SUPERVISOR_VER=3.0
-WHEEL_VER=0.23.0
+SUPERVISOR_VER=3.1.2
+WHEEL_VER=0.24.0
 
 
 ### Pypi mirros
--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,35 @@
 #!/usr/bin/env bash
 
+# !NOTE! You have to run this script in sudo mode
+
+# Change yum mirror
+sed -i.backup 's/^enabled=1/enabled=0/' /etc/yum/pluginconf.d/fastestmirror.conf
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+wget -O /etc/yum.repos.d/CentOS-Base-aliyun.repo http://mirrors.aliyun.com/repo/Centos-6.repo
+yum makecache
+yum -y update
+yum -y install wget
+
+# Install EPEL repository
+wget -P ~/ http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+wget -P ~/ http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+rpm -Uvh ~/remi-release-6*.rpm ~/epel-release-6*.rpm
+
+# EPEL Mirror
+mv /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel.repo.backup 2>/dev/null || :
+mv /etc/yum.repos.d/epel-testing.repo /etc/yum.repos.d/epel-testing.repo.backup 2>/dev/null || :
+wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-6.repo
+
+# Disable iptables
+iptables -F
+
+service iptables stop
+chkconfig iptables off
+
+# Install build toolchain
+yum -y groupinstall "Development tools"
+yum -y install openssl-devel readline-devel bzip2-devel sqlite-devel zlib-devel ncurses-devel db4-devel expat-devel
+
 # Install ruby dependencies
 yum -y install ruby rubygems ruby-devel
 # Use taobao repo for ruby gems
@@ -7,8 +37,4 @@ gem sources -a http://ruby.taobao.org/
 # Remove origin repo from ruby gems
 gem sources --remove http://rubygems.org/
 # Install fpm
-gem install fpm
-
-# Install build toolchain
-yum -y groupinstall "Development tools"
-yum -y install openssl-devel readline-devel bzip2-devel sqlite-devel zlib-devel ncurses-devel db4-devel expat-devel
+gem install fpm -v 1.0.2
